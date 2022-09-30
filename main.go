57e9e9916526e33a7f312f1946f4c08a2fddf70e
infoengine1337 <infoengine1337@gmail.com>
2022-09-30 12:17:21 +0900

    initialize

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/imroc/req/v3"
+)
+
+func main() {
+
+	before := time.Now()
+	fmt.Println("[*] Starting Program...")
+
+	var wg sync.WaitGroup
+
+	for i := 0; i <= 3000; i++ {
+
+		wg.Add(1)
+		go func() {
+
+			defer wg.Done()
+
+			client := req.C()
+			resp, err := client.R(). // Use R() to create a request.
+							Get("http://localhost:8000")
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println(resp)
+
+		}()
+
+	}
+
+	wg.Wait()
+
+	after := time.Now()
+
+	fmt.Printf("[*] It takes %f second.\n", (after.Sub(before)).Seconds())
+}
